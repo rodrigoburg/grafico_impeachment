81f93dcbb87b40e1576c89c27b5db17c642d60fd
Rodrigo Burgarelli <rodrigo.burgarelli1@gmail.com>
2016-04-17 21:13:58 -0300

    agora funfando

--- candidatos.js
+++ candidatos.js
@@ -4,17 +4,34 @@ function inicializa() {
 
     var svg = dimple.newSvg("#candidatos", 1400, 1000);
     d3.csv("dados_perfil.csv", function (data) {
-        //data = dimple.filterData(data, "candidato", ["total","dilma"])
+        data = dimple.filterData(data, "existe", "TRUE")
+        data.forEach(function (d) {
+            d.governismo = parseFloat(d.governismo)
+        })
+        var cores_default = [
+            "#A11217",
+            "#00408F",
+            "#E9BC00"
+        ]
         //data = dimple.filterData(data, "categoria", "idade")
         var myChart = new dimple.chart(svg, data);
         myChart.setBounds(110, 30, 1900, 700);
-        myChart.addMeasureAxis("y", "valor");
-        var y = myChart.addCategoryAxis("x", ["dado","candidato"]);
-        y.title = ""
-        series = myChart.addSeries("candidato", dimple.plot.bar);
+        myChart.addMeasureAxis("y", "governismo");
+        var x = myChart.addCategoryAxis("x", "nome_impeachment");
+
+        series = myChart.addSeries(["nome_impeachment","posicao"], dimple.plot.bar);
         series.barGap = 0.42;
-        y.overrideMin = 3500;
+        myChart.assignColor("contra", cores_default[0],cores_default[0]);
+        myChart.assignColor("a favor", cores_default[1],cores_default[1]);
+        myChart.assignColor("abstencao", cores_default[2],cores_default[2]);
+        myChart.assignColor("ausencia", cores_default[2],cores_default[2]);
+
+
+
         myChart.draw();
+        x.shapes.selectAll("text").remove();
+
+        console.log(data)
 
     });
 }
